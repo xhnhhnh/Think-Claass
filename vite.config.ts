68cd5b0d52ab03a6bd42605611fe72dfc100c370
xhnhhnh <xhnhhnh@users.noreply.github.com>
2026-04-06 05:20:25 +0000

    fix: remove react-dev-locator to prevent <no value> console spam on homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
-      },
-    }),
+    react(),
     tsconfigPaths(),
   ],
   server: {
